6911be2ff406f79ed4b2656a9496730ef14442a8
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-09-19 11:50:15 -0500

    Update Past.php

--- Past.php
+++ Past.php
@@ -20,7 +20,11 @@
 
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+            <li class="breadcrumb-item"><a href=
+<?php
+echo $_GET["txt"];
+?>
+>Home</a></li>
             <li class="breadcrumb-item active" aria-current="page">Past Races</li>
         </ol>
     </nav>
