a4bc81a64314f4f18b0233d85436da5dd9f24142
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-09-26 14:49:53 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -3,6 +3,6 @@
 <?php include 'header.php'; ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <body>
-Welcome <?php echo $_POST["fname"]; echo $_GET["lname"]; ?>, Thank You for Visiting My Website
+Welcome <?php echo $_POST["fname"]; ?>, Thank You for Visiting My Website. I think that <?php echo $_GET["lname"]; ?> is a cool last name.
 </body>
 </html>
