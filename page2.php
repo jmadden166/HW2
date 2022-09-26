6f030ae31c9ffa6d1f9a10048c2d086f19e1234d
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-09-26 14:48:14 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -2,5 +2,7 @@
 <html lang="en">
 <?php include 'header.php'; ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
- <h2> Hello <?php first(); last(); ?>, Thank You for Visiting My Site </h2>
+<body>
+Welcome <?php echo $_POST["fname"]; echo $_GET["lname"]; ?>, Thank You for Visiting My Website
+</body>
 </html>
