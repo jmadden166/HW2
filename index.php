19e61aa0dbb629636e0c2e822b92a4c364a6b195
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-09-26 14:26:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
                <?php
   function getTime() {
-  $timezone = date("Y/m/d");
+  $timezone = date("m/d/y");
 echo $timezone;
   }
             
