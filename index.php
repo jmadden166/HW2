17b6aa03aa051f9085ff2d126eab093b670a57e3
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-09-26 14:26:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
                <?php
   function getTime() {
-  $timezone = date_default_timezone_get();
-echo "The current server timezone is: " . $timezone;
+  $timezone = date("Y/m/d");
+echo $timezone;
   }
             
   ?>
-        <h2>It is currently <?php getTime(); ?></h2>
+        <h2>Today's Date is <?php getTime(); ?></h2>
     </body>
     <div class="dropdown">
         <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
