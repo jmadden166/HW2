247887fd515f63ae06dac9967a4ddc4b237c93a6
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-09-26 13:57:31 -0500

    Update home.php

--- home.php
+++ home.php
@@ -17,16 +17,18 @@
             }
         </style>
     </head>
-     <?php
+
+    <body>
+        <h1>Welcome to My Website</h1>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+               <?php
   function getTime() {
   $timezone = date_default_timezone_get();
 echo "The current server timezone is: " . $timezone;
   }
-  ?>
-    <body>
-        <h1>Welcome to My Website</h1>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-          <h2>It is currently getTime()</h2>
+ 
+        <h2>It is currently getTime()</h2>
+             ?>
     </body>
  
 
