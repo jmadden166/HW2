5946dfb024d77a864dfe25b7c594eb219162785f
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-09-26 14:17:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <!doctype html>
 <html lang="en">
 <?php include 'header.php'; ?>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Homework 2</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <head>
-        <style>
-            h1 {
-                text-align: center;
-            }
 
-            h2 {
-                text-align: center;
-            }
-        </style>
-    </head>
 
     <body>
         <h1>Welcome to My Website</h1>
