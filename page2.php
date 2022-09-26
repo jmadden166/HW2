b623c9377401baac7a595937258039bdd4b129a5
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-09-26 14:56:02 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -4,6 +4,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <body>
 Welcome <?php echo $_POST["fname"]; ?>, Thank You for Visiting My Website.<br>
-Your email address is:<?php echo $_GET["email"]; ?>
+Your email address is: <?php echo $_POST["email"]; ?>
 </body>
 </html>
