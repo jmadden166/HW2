ccfac698fbff1a3c97b1b38fc8753acdc757f7fc
jmadden166 <78659489+jmadden166@users.noreply.github.com>
2022-09-26 14:59:51 -0500

    Update and rename page3.php to page3_get.php

---
+++ page3_get.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="en">
+<?php include 'header.php'; ?>
+Your Favorite Class is: <?php echo $_GET["fClass"]; ?><br>
+Your Favorite Teacher is: <?php echo $_GET["teach"]; ?>
+
+</body>
+</html> 
